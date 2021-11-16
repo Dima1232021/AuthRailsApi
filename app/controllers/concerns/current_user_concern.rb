e1605fc4db73a6ed755e6b8a5cd1ebb39f21acfb
Dima1232021 <moarobota584@gmail.com>
2021-11-16 23:29:30 +0200

    firs commit

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -4,8 +4,6 @@ module CurrentUserConcern
   included { before_action :set_current_user }
 
   def set_current_user
-    puts 'in currentUserConcern set_current_user||||||||||||||||||||||||||||||||||||'
-    puts session[:user_id]
     @current_user = User.find(session[:user_id]) if session[:user_id]
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,15 +8,18 @@ Bundler.require(*Rails.groups)
 
 module AuthenticationApp
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.action_dispatch.cookies_same_site_protection = :lax
+
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore,
+                          key: '_authentication_app',
+                          expire_after: 20.years
+    config.middleware.insert_after(
+      ActionDispatch::Cookies,
+      ActionDispatch::Session::CookieStore,
+      key: '_authentication_app',
+    )
   end
 end
